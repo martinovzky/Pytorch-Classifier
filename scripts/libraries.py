3c3c85e20a3ef71823a85e845a6bca50beca5c34
Martin Swann <162488674+martinovzky@users.noreply.github.com>
2025-02-28 10:16:04 +0100

    Update libraries.py

--- scripts/libraries.py
+++ scripts/libraries.py
@@ -1,1 +1,14 @@
+import torch as torch
+import torch.nn as nn         # classes and functions for NN building
+import torch.optim as optim   # optimization algorithms for training
+from torch.utils.data import Dataset, DataLoader
+import torchvision            # pre-trained models
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder # for batching and loading data
+import timm                                  # contains the models
 
+import matplotlib.pyplot as plt              # for data viz
+import pandas as pd
+import numpy as np
+import sys
+from tqdm.notebook import tqdm
