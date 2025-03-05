e87801fb29e638017370ca4c6f6f92558d225f36
Martin Swann <162488674+martinovzky@users.noreply.github.com>
2025-03-05 11:41:45 +0100

    Update 02_hyperparameters.py

--- scripts/02_hyperparameters.py
+++ scripts/02_hyperparameters.py
@@ -1,5 +1,8 @@
 batch = 32               # of images / training batch
-epochs = 10              # tot number of training epochs (full passes over the dataset)
-LearningRate = 1e-3      # learning rate for the optimizer (how much should model weights be updated after each batch )
+number_epochs = 15       # tot number of training epochs (full passes over the dataset)
+LearningRate = 1e-3      # learning rate for the optimizer (how much should model weights be updated after each batch)
+WeightDecay = 1e-5       # discourages large parameters in order not to overfit
+Gamma = 0.1              # factor by which the learning rate will be reduced, helpw the model converge more smoothly and avoid overshooting local minima
 NumClasses = 67          # MIT‑67 dataset has 67 scene categories
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
