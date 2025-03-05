31ec9730be10bdd978c78ead8b7ee176f58074ce
Martin Swann <162488674+martinovzky@users.noreply.github.com>
2025-03-05 11:43:53 +0100

    Update 04_model_setup.py

--- scripts/04_model_setup.py
+++ scripts/04_model_setup.py
@@ -8,8 +8,13 @@ model.classifier = nn.Linear(in_features, NumClasses)  # replaces with a new lin
 
 model = model.to(device)
 
+
 #loss function
 criterion = nn.CrossEntropyLoss()
 
 #optimizer updates model weights during training
-optimizer = optim.Adam(model.parameters(), lr=LearningRate)
+optimizer = optim.Adam(model.parameters(), lr=LearningRate, weight_decay=WeightDecay)
+
+# learning rate schedule, reduces LR after every 5 epochs
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=Gamma)
+# after every 5 epochs, the LR is multiplied by gamma=0.1
